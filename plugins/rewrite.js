1f966f0cdb82f8a7d3c218067035b79f71453b04
pooya parsa <pooya@pi0.ir>
2019-02-21 12:16:42 +0330

    use history api when possible

--- plugins/rewrite.js
+++ plugins/rewrite.js
@@ -1,12 +1,20 @@
-export default function ({ req, beforeNuxtRender, app }) {
+// Detect rewrites in client-side and update browser URL in that case
+if (process.client) {
+  const { pathname } = window.__NUXT__
+  if (pathname && window.location.pathname !== pathname) {
+    if (typeof history !== 'undefined') {
+      history.replaceState(null, null, pathname)
+    } else {
+      window.location = pathname
+    }
+  }
+}
+
+export default function ({ req, beforeNuxtRender }) {
+  // Preserve original requested URL in nuxtState
   if (process.server) {
     beforeNuxtRender(({ nuxtState }) => {
       nuxtState.pathname = req.url
     })
-  } else {
-    const { pathname } = window.__NUXT__
-    if (pathname && window.location.pathname !== pathname) {
-      window.location = pathname
-    }
   }
 }
